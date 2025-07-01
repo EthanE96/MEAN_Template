833e55bc8a3e3a9c0d24646a20c2ea3223e2ee36
Ethan <EthanAEdwards5@outlook.com>
2025-07-01 10:20:33 -0600

    profile data

--- ui/src/app/pages/home/profile/profile.component.ts
+++ ui/src/app/pages/home/profile/profile.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
 import { IUser } from '../../../models/user.model';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
@@ -14,37 +13,20 @@ import ValidatorUtils from '../../../utils/validator.utils';
   imports: [NgIf, FormsModule, MessageComponent],
   templateUrl: './profile.component.html',
 })
-export class ProfileComponent implements OnInit, OnDestroy {
-  currentUser$ = new Observable<Partial<IUser> | null>();
+export class ProfileComponent implements OnInit {
   user: Partial<IUser> | null = null;
-  originalUser: Partial<IUser> | null = null;
-
   errorMessage: string | null = null;
   successMessage: string | null = null;
-
-  private userSub?: Subscription;
-
-  // TODO: The update profile logic is not complete. Data only updates on refresh.
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    this.currentUser$ = this.authService.currentUser$;
-
-    this.userSub = this.currentUser$.subscribe((user) => {
-      // Deep copy to avoid reference issues
-      this.originalUser = user ? JSON.parse(JSON.stringify(user)) : null;
-      this.user = user ? JSON.parse(JSON.stringify(user)) : null;
-    });
-  }
-
-  ngOnDestroy() {
-    this.userSub?.unsubscribe();
+    this.user = this.authService.currentUserSubject.value;
   }
 
   async updateProfile() {
     try {
-      if (!this.user || !this.originalUser) {
+      if (!this.user) {
         throw new Error('No user data available to update.');
       }
 
@@ -62,15 +44,6 @@ export class ProfileComponent implements OnInit, OnDestroy {
       // Validate email
       if (!ValidatorUtils.isValidEmail(this.user.email)) {
         throw new Error('Enter valid email address.');
-      }
-
-      // If no changes, return early
-      if (
-        this.user.firstName == this.originalUser.firstName &&
-        this.user.lastName == this.originalUser.lastName &&
-        this.user.email == this.originalUser.email
-      ) {
-        throw new Error('No changes to update.');
       }
 
       await this.authService.updateUser(this.user);
