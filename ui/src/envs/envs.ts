a2306bdf7bf4aace7dcbe9699da4c4ade711e5c3
Ethan <EthanAEdwards5@outlook.com>
2025-07-28 14:57:22 -0600

    Workflows

--- ui/src/envs/envs.ts
+++ ui/src/envs/envs.ts
@@ -3,5 +3,4 @@ export const environment = {
   production: true,
   apiUrl: 'https://mean-app-axdqhsa0cbbme9cc.westus-01.azurewebsites.net/api',
   documentationUrl: 'https://icy-field-05d86561e.1.azurestaticapps.net',
-  clarityProjectId: '', // Will be injected by CI/CD
 };
