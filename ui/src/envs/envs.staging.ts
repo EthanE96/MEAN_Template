ed984018cfdcc80abd494de6e4a7092a08db6562
Ethan <EthanAEdwards5@outlook.com>
2025-07-28 12:18:43 -0600

    Revert "Delete ui/src/envs directory"
    This reverts commit 515759f07abe1b5846d0fcdef8daab11ce5e5b3b.

---
+++ ui/src/envs/envs.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  // Staging ENV
+  production: true,
+  apiUrl: '',
+};
---
+++ ui/src/envs/envs.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  //  Production ENV
+  production: true,
+  apiUrl: 'https://mean-app-axdqhsa0cbbme9cc.westus-01.azurewebsites.net/api',
+};
