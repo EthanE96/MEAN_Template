b7ca19568a75c0c76fc02301777ded448ba92ced
Ethan <EthanAEdwards5@outlook.com>
2025-07-28 14:34:09 -0600

    UI Workflows

--- ui/src/envs/envs.example.ts
+++ ui/src/envs/envs.example.ts
@@ -1,6 +1,7 @@
 export const environment = {
   // Envs example
   production: false,
+  // This secret should be injected at build time workflow as Github Secrets
   clarityProjectId: '',
   apiUrl: '',
   documentationUrl: '',
---
+++ ui/src/envs/envs.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  // Production ENV
+  // Inject clarityProjectId secret into this file
+  production: true,
+  apiUrl: 'https://mean-app-axdqhsa0cbbme9cc.westus-01.azurewebsites.net/api',
+  documentationUrl: 'https://icy-field-05d86561e.1.azurestaticapps.net',
+};
