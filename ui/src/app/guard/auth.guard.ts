dbacbe41e23cd66a9615139cc1b4139f93f80374
Ethan <EthanAEdwards5@outlook.com>
2025-08-04 21:26:36 -0600

    UI Redirect

--- ui/src/app/guard/auth.guard.ts
+++ ui/src/app/guard/auth.guard.ts
@@ -1,10 +1,11 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = async () => {
   // Inject services inside the function
   const authService = inject(AuthService);
+  const router = inject(Router);
 
   // Check if the user is authenticated via cache / behavior subject
   if (authService.isAuthenticated()) return true;
@@ -13,5 +14,7 @@ export const authGuard: CanActivateFn = async () => {
   await authService.getSession().catch(() => {});
   if (authService.isAuthenticated()) return true;
 
+  // Redirect to home page if not authenticated
+  await router.navigate(['/']);
   return false;
 };
